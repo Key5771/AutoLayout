40fd6337da5be0aa272e87cd2c9af8295b1bbbd2
key5771 <ksj57715@hanmail.net>
2020-08-06 17:40:40 +0900

    Add View, Label, TextField

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    lazy var box = UIView()
+    lazy var titleLabel = UILabel()
+    lazy var textField = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print("frame: \(self.view.frame)")
+        
+        self.view.addSubview(box)
+        box.backgroundColor = UIColor(displayP3Red: 252/256, green: 251/256, blue: 234/256, alpha: 1.0)
+        box.snp.makeConstraints { (make) in
+            make.width.height.equalTo(self.view)
+            make.center.equalTo(self.view)
+        }
+        
+        self.view.addSubview(titleLabel)
+        titleLabel.text = "로그인"
+        titleLabel.textAlignment = .center
+        
+        let size = self.view.frame.height * 0.1
+        
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(size)
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-30)
+        }
+        
+        self.view.addSubview(textField)
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.placeholder = "test"
+        textField.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(100)
+            make.trailing.equalToSuperview().offset(-100)
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(size)
+            make.centerX.equalTo(self.view)
+        }
     }
 
 
